1a8cab94c7e7773c13a41f0686c25ae545141b8d
Marta Lleonsí <u1901735@correu.udg.edu>
2018-09-24 18:36:18 +0200

    tests

--- lab/junithelloworld/src/test/java/edu/udg/caes/ExercicesIntroTestingTest.java
+++ lab/junithelloworld/src/test/java/edu/udg/caes/ExercicesIntroTestingTest.java
@@ -13,9 +13,27 @@ public class ExercicesIntroTestingTest {
     }
 
     @Test
+    public void testFindLast2(){
+        final int result = ExercicesIntroTesting.findLast(new int []{2, 3, 5, 2}, 2);
+        assertEquals(result, 3);
+    }
+
+    @Test
+    public void testFindLastWorkingWithWrongCode(){
+        final int result = ExercicesIntroTesting.findLast(new int []{1, 3, 2}, 2);
+        assertEquals(result, 2);
+    }
+
+    @Test
     public void testLastZero(){
         final int result = ExercicesIntroTesting.lastZero(new int []{0, 1, 0});
         assertEquals(result, 2);
+    }
+
+    @Test
+    public void testLastZeroWorkingWithWrongCode(){
+        final int result = ExercicesIntroTesting.lastZero(new int []{1, 3, 1});
+        assertEquals(result, -1);
     }
 
     @Test
@@ -25,8 +43,26 @@ public class ExercicesIntroTestingTest {
     }
 
     @Test
+    public void testCountPositiveWorkingWithWrongCode(){
+        final int result = ExercicesIntroTesting.countPositive(new int []{-4, -2, -1, -2});
+        assertEquals(result, 0);
+    }
+
+    @Test
     public void testOddOrPos(){
         final int result = ExercicesIntroTesting.oddOrPos(new int []{-3, -2, 0, 1, 4});
         assertEquals(result, 3);
+    }
+
+    @Test
+    public void testOddOrPos2(){
+        final int result = ExercicesIntroTesting.oddOrPos(new int []{0, -1, -2, -3, -4});
+        assertEquals(result, 2);
+    }
+
+    @Test
+    public void testOddOrPosWorkingWithWrongCode(){
+        final int result = ExercicesIntroTesting.oddOrPos(new int []{0, 0, 0, 0, 0});
+        assertEquals(result, 0);
     }
 }
